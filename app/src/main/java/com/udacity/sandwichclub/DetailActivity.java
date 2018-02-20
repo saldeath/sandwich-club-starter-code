d3e91fc2e20773c24da6e842dd8042e87b0a6ebe
saldeath <seonden@gmail.com>
2018-02-20 11:36:33 +0100

    detail layout styling

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -15,12 +16,18 @@ public class DetailActivity extends AppCompatActivity {
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
 
+    private TextView mTextViewAlsoKnownAs, mTextViewIngredients, mTextViewPlaceOfOrigin, mTextViewDescription;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
+        mTextViewAlsoKnownAs = findViewById(R.id.also_known_tv);
+        mTextViewIngredients = findViewById(R.id.ingredients_tv);
+        mTextViewPlaceOfOrigin = findViewById(R.id.origin_tv);
+        mTextViewDescription = findViewById(R.id.description_tv);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -43,7 +50,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +63,10 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
-
+    private void populateUI(Sandwich sandwich) {
+        mTextViewAlsoKnownAs.setText(sandwich.getAlsoKnownAs().toString());
+        mTextViewIngredients.setText(sandwich.getIngredients().toString());
+        mTextViewPlaceOfOrigin.setText(sandwich.getPlaceOfOrigin());
+        mTextViewDescription.setText(sandwich.getDescription());
     }
 }
